711a6c7592ea373c99a1ba944f3b312d9ea10002
Richard Soriano <59493601+richardsoriano@users.noreply.github.com>
2020-09-09 12:52:46 -0700

    Add files via upload

---
+++ server/client/build/precache-manifest.3f172bd6ebbc0d40fc3b77e0ed873ba7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75a84e383ef884cf15eb58c6b5d0cf36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e169b48135acd735411f",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "e169b48135acd735411f",
+    "url": "/static/js/2.91137bcd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.91137bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d27565d94d57b2622604",
+    "url": "/static/js/main.003a4b55.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
