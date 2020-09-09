f7e14c5acd8db5052221d86bdf6fa3e432e9508e
Richard Soriano <59493601+richardsoriano@users.noreply.github.com>
2020-09-09 12:31:19 -0700

    Add files via upload

---
+++ server/client/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import surveysReducer from "./surveyReducers";
+
+import { reducer as reduxForm } from "redux-form";
+
+export default combineReducers({
+  auth: authReducer,
+  form: reduxForm,
+  surveys: surveysReducer,
+});
